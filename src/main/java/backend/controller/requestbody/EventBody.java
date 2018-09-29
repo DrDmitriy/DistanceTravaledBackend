e8db7bf87069cff1243eb5e1818200643f9a7eea
DrDmitriy <dr_dmitriy96@mail.ru>
2018-09-29 15:29:58 +0300

    code clean

--- src/main/java/backend/controller/requestbody/EventBody.java
+++ src/main/java/backend/controller/requestbody/EventBody.java
@@ -4,19 +4,14 @@ import backend.entity.Category;
 import backend.entity.UserEntity;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
-import java.util.HashSet;
 import java.util.Set;
 
 @NoArgsConstructor
-// @AllArgsConstructor
 @Data
-// @Builder
 public class EventBody {
     private Long userId;
     private String eventName;
@@ -57,19 +52,3 @@ public class EventBody {
     }
 
 }
-
-
-/*
-    private Timestamp stringDateToTimestamp(String startEvent) {
-        String[] date = startEvent.split("\\.");
-        if (date != null && date.length == 5) {
-            int year = Integer.valueOf(date[0]);
-            int month = Integer.valueOf(date[1]);
-            int day = Integer.valueOf(date[2]);
-            int hour = Integer.valueOf(date[3]);
-            int min = Integer.valueOf(date[4]);
-            return new Timestamp(year - 1900, month - 1, day, hour, min, 0, 0);
-        } else {
-            return null;
-        }
-    }*/
